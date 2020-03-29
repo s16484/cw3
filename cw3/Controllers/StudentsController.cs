2f4dc9c890343700e1fbcef8fec05b0d1ba00ce3
Michał Bartosiak <s16484@pjwstk.edu.pl>
2020-03-29 17:31:31 +0200

    zad6

--- cw3/Controllers/StudentsController.cs
+++ cw3/Controllers/StudentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using cw3.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace cw3.Controllers
@@ -10,11 +11,43 @@ namespace cw3.Controllers
     [Route("api/students")]
     public class StudentsController : ControllerBase
     {
-        [HttpGet]
 
-        public string GetStudent(string orderBy)
+/*        [HttpGet]
+        public string GetStudents()
         {
-            return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+            return "Kowalski, Malewski, Andrzejewski";
+        }
+*/
+        [HttpGet("{id}")]
+        public IActionResult GetStudentDetails(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            }
+            else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+            return NotFound("Nie znaleziono studenta");
+        }
+
+       [HttpGet]
+
+       public string GetStudentSort(string orderBy)
+       {
+           return $"Kowalski, Malewski, Andrzejewski sortowanie={orderBy}";
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            // add to database
+            // genrating index number
+
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+
+            return Ok(student);
         }
 
     }
